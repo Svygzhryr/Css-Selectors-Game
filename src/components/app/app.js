3f91f14018e2462b3393a2a7178ad02a0c15ec9c
Svygzhryr <pamixy@gmail.com>
2023-06-14 10:58:40 +0300

    chore: webpack and basic settings add

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+class App {
+    constructor() {
+        this.message = message;
+    }
+
+    start() {
+        this.message = 'lmao';
+        console.log(this.message);
+    }
+}
+
+export default App;
