91fb035b018030b2a610017f8fa9d29d51dd5a2e
Svygzhryr <pamixy@gmail.com>
2023-06-14 11:46:16 +0300

    chore: sass add

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,14 @@
 class App {
-    constructor() {
+    constructor(message) {
         this.message = message;
     }
 
     start() {
-        this.message = 'lmao';
-        console.log(this.message);
+        this.message = 'hi';
+        console.log(hi);
+        let a = document.createElement('div');
+        a.className = 'a';
+        document.body.appendChild(a);
     }
 }
 
